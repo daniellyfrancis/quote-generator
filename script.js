88df7a5b02747d3d24e53550f2b9df541a1badf6
daniellyfrancis <francisdanielly04@gmail.com>
2024-09-23 07:26:25 +0100

    Get Quotes from API

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('testing');
+// Get Quotes From API
+let apiQuotes = [];
+
+// Show New Quote;
+function newQuote() {
+    // Pick a random quote from apiQuotes array;
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+};
+
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
